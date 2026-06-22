aa6ff4f09d42eae4c47bfd43962ace23cf48fe0e
carlinho011 <carloperrone011@gmail.com>
2026-06-22 17:33:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if "autenticato" not in st.session_state: st.session_state["autenticato"] = Fals
 
 # --- LOGIN ---
 if not st.session_state["autenticato"]:
-    st.title("Area Riservata 💜")
+    st.title("Area Riservata Docenti")
     nome = st.text_input("Nome Docente:")
     pw = st.text_input("Password:", type="password")
     if st.button("Accedi"):
